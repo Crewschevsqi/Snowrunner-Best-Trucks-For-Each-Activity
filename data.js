603baafe16808601881da99b1f42bb8a0e33b9a7
Crewschevsqi <maksymilian.kru@proton.me>
2026-09-02 23:44:06 +0200

    dasdas

--- data.js
+++ data.js
@@ -55,16 +55,16 @@ const VEHICLES = [
   { id: "zikz_612h_water", name: "ZikZ 612H", category: "Water Transportation", unlocked: false, region: "Mastodon DLC", image: "images/Zikz 612h.jpg" },
 
   // -- Overloading --
-  { id: "kenworth_963_overloading", name: "Kenworth 963", category: "Overloading", unlocked: false, region: "Duncan Bay, British Columbia", image: "" },
-  { id: "zikz_612h_overloading", name: "ZikZ 612H", category: "Overloading", unlocked: false, region: "", image: "" },
-  { id: "azov_73210_overloading", name: "Azov 73210", category: "Overloading", unlocked: false, region: "", image: "" },
-  { id: "azov_atom_overloading", name: "Azov Atom", category: "Overloading", unlocked: false, region: "", image: "" },
-  { id: "paystar_5600ts_overloading", name: "Paystar 5600 TS", category: "Overloading", unlocked: false, region: "", image: "" },
+  { id: "kenworth_963_overloading", name: "Kenworth 963", category: "Overloading", unlocked: false, region: "Duncan Bay, British Columbia", image: "images/kennyover.jpg" },
+  { id: "zikz_612h_overloading", name: "ZikZ 612H", category: "Overloading", unlocked: false, region: "Mastodon DLC", image: "images/612hover.jpg" },
+  { id: "azov_73210_overloading", name: "Azov 73210", category: "Overloading", unlocked: true, region: "", image: "images/azov73210over.jpg" },
+  { id: "azov_atom_overloading", name: "Azov Atom", category: "Overloading", unlocked: false, region: "Azov Atom DLC", image: "images/atomover.png" },
+  { id: "paystar_5600ts_overloading", name: "International Paystar 5600 TS", category: "Overloading", unlocked: false, region: "Grainwoods River, Wisconsin", image: "images/5600over.jpg" },
 
   // -- Special Trailers --
   { id: "zikz_605r_trailers", name: "ZikZ 605R", category: "Special Trailers", unlocked: false, region: "Cosmodrome, Amur", image: "images/zikz605r.jpg" },
   { id: "zikz_612h_trailers", name: "ZikZ 612H", category: "Special Trailers", unlocked: false, region: "Mastodon DLC", image: "images/zikzspecial.jpg" },
-  { id: "plad_450_trailers", name: "Plad 450", category: "Special Trailers", unlocked: false, region: "Zherbai Quarries, Almaty", image: "plad450.jpg" },
+  { id: "plad_450_trailers", name: "Plad 450", category: "Special Trailers", unlocked: false, region: "Zherbai Quarries, Almaty", image: "images/plad450.jpg" },
   { id: "kolob_74760_trailers", name: "Kolob 74760", category: "Special Trailers", unlocked: true, region: "", image: "images/kolob74760.jpg" },
   { id: "derry_special_trailers", name: "Derry Special 15C-177", category: "Special Trailers", unlocked: false, region: "The Albany River, Ontario", image: "images/derry15c.jpg" },
 
